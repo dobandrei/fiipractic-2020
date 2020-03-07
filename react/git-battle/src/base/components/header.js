69f4afc5543dd5fb258fb64822158631dc6ded52
dob_andrei <dob_andrei@yahoo.com>
2020-03-07 17:07:03 +0200

    Sambata 07.03.2020

---
+++ react/git-battle/src/base/components/header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import '../styles/style.css'
+
+class Header extends React.Component {
+    render(){
+        return (
+            <div className = 'header'> 
+                <p> I am the HEADER !</p>
+            </div>
+        )
+    }
+}
+
+export default Header
